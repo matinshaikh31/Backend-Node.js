2814f2f5881d18b73ac92d23ef6abfd34ee8e006
matinshaikh31 <118182648+matinshaikh31@users.noreply.github.com>
2024-09-02 13:56:33 +0530

    jwt

--- 24_JWT_Authentication_NodeJs/service/auth.js
+++ 24_JWT_Authentication_NodeJs/service/auth.js
@@ -1,20 +1,26 @@
-const jwt = require("jsonwebtoken")
-const secret = "Matinshaikh31"
+const jwt = require("jsonwebtoken");
+const secret = "Matinshaikh31";
 
-function setUser( user){
-   return jwt.sign({
-    _id:user._id,
-    email:user.email,
-    
-   },secret)
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(token){
-    if(!token) return null
-   return jwt.verify(token,secret)
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (err) {
+    return null;
+  }
 }
 
-module.exports={
-    setUser,
-    getUser
+module.exports = {
+  setUser,
+  getUser,
 };
